630fecc62df24a02ba3f58806b8e44b0b19e5432
asifminhaz <minhazurrahmanasif40@gmail.com>
2022-05-23 17:28:17 +0600

    load all data

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config()
+const port = process.env.PORT || 5000
+
+//middleware//
+app.use(cors())
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.yya0r.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+ async function run (){
+           try{
+          await client.connect();
+          const toolCollection = client.db('motoparts').collection('tools')
+
+          app.get('/tool', async(req, res) => {
+                    const query = {}
+                    const cursor = toolCollection.find(query)
+                    const tools = await cursor.toArray();
+                    res.send(tools)
+          })
+           }
+           finally{
+
+           }
+ }
+
+run().catch(console.dir)
+app.get('/', (req, res) => {
+  res.send('hello world')
+})
+
+app.listen(port, () => {
+          console.log('motoparts is on', port)
+})
