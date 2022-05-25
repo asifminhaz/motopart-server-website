c69a29e6268cd528073a07d004fd0172dec5450c
asifminhaz <minhazurrahmanasif40@gmail.com>
2022-05-26 01:31:36 +0600

    add isadmin

--- index.js
+++ index.js
@@ -108,6 +108,13 @@ function verifyJWT (req, res, next){
                     res.send(result)
                 })
 
+                app.get('/admin/:email', async (req, res)=> {
+                  const email = req.params.email;
+                  const user = await userCollection.findOne({email: email});
+                  const isAdmin = user.role === 'admin';
+                  res.send({admin : isAdmin})
+                })
+
                
 
                 app.put('/user/admin/:email',  async (req, res) => {
