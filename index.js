c6f5a628602e39af1c90ad112900f76550de1ea0
asifminhaz <minhazurrahmanasif40@gmail.com>
2022-05-26 01:27:52 +0600

    add admin api

--- index.js
+++ index.js
@@ -14,6 +14,20 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+function verifyJWT (req, res, next){
+          const authHeader = req.headers.authorization;
+          if(!authHeader){
+            return res.status(401).send({message: 'Unauthorize access'})
+          }
+          const token = authHeader.split(' ')[1]
+          jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function(err, decoded){
+            if(err){
+              return res.status(403).send({message: 'Forbidden access'})
+            }
+            req.decoded = decoded;
+            next()
+          })
+}
  async function run (){
            try{
           await client.connect();
@@ -21,11 +35,20 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
           const userCollection = client.db('motoparts').collection('users')
           const ordersCollection = client.db('motoparts').collection('orders')
 
-          app.get('/orders/:email', async(req, res)=> {
+          app.get('/orders/:email', verifyJWT, async(req, res)=> {
                     const email = req.params.email;
-                    const query = {email: email}
-                    const orders = await ordersCollection.find(query).toArray()
-                    res.send(orders)
+                    // const authorization = req.headers.authorization
+                    const decodedEmail = req.decoded.email;
+                    if(email === decodedEmail){
+                      const query = {email: email}
+                      const orders = await ordersCollection.find(query).toArray()
+                      res.send(orders)
+
+                    }
+                    else{
+                      return res.status(403).send({message: 'Forbidden access'})
+                    }
+                   
 
           })
           app.get('/tool', async(req, res) => {
@@ -41,6 +64,30 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
                     res.send(tool);
                 });
 
+                app.get('/user', verifyJWT, async (req, res)=> {
+                  const users = await userCollection.find().toArray()
+                  res.send(users)
+                })
+
+                app.put('/user/admin/:email', async (req, res) => {   
+                    const email = req.params.email;     
+                    const initiator = req.decoded.email
+                    const initiatorAccount = await userCollection.findOne({email: initiator})
+                    if(initiatorAccount.role === 'admin')    {
+                      const filter = { email: email };
+                      const updatedDoc = {
+                        $set: {role: 'admin'},
+                      };
+                            const result = await userCollection.updateOne(filter, updatedDoc)
+                      
+                            res.send(result)
+
+                    }       
+                    else{
+                      res.status(403).send({message: "Forbidden"})
+                    }
+                   
+                })
                 app.put('/user/:email', async (req, res) => {
                     const name = req.body      
                     const email = req.params.email;
